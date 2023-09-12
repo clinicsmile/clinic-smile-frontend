7935cebb02d28b833ee1b5840adc649abae299bb
AndresMaya13 <andresmaya3113@gmail.com>
2023-09-11 22:06:30 -0500

    home width fix

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="flex w-sceen h-screen items-center">
-      <div className="w-4/5 h-5/6 m-6 bg-white rounded-3xl p-6">
+      <div className="h-5/6 m-6 bg-white rounded-3xl p-6">
         <h1 className="text-6xl h-full	w-full text-center text-[#1783B0]">
           Hola Usuario, Bienvenido a ClinicSmile
         </h1>
