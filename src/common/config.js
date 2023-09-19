abdba51eeb0782c74d9ec8a404fc5925ee9d0e57
AndresMaya13 <andresmaya3113@gmail.com>
2023-09-19 01:10:28 -0500

    config env fix

--- src/common/config.js
+++ src/common/config.js
@@ -1,4 +1,3 @@
-const ENV = "development";
 const configPath = `./env/development.json`;
-const config = await (async () => await import(configPath))();
+const config = import(configPath);
 export const API = { name: "clinic-smile-api", url: config.CLINIC_SMILE_API };
