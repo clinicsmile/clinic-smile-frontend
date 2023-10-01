f5f8d14d75e3a9b3b818f090e0a221ae2acb3573
Harold Jaramillo <harold.jaramillo@utp.edu.co>
2023-10-01 16:59:45 -0500

    Se agrega la busqueda por numero de documento

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,4 +1,4 @@
-import { Table, Button, Modal } from "flowbite-react";
+import { Table, Button, Modal, Spinner, TextInput } from "flowbite-react";
 import { useState, useEffect } from "react";
 import { AppForm } from "../../components/form/AppForm";
 import { Forms } from "../../data/form/Forms";
@@ -7,7 +7,6 @@ import { validate } from "../../middlewares/validateLogin";
 import { services } from "../../services/services";
 import Swal from "sweetalert2";
 import ModalComponent from "../../components/modal/Modal";
-import { Spinner, Tabs } from "flowbite-react";
 
 function Users() {
   const [openModal, setOpenModal] = useState(false);
@@ -31,6 +30,8 @@ function Users() {
   useEffect(() => {
     getUsers();
   }, []);
+
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleToggleModal = (type = "", user = {}) => {
     setCurrentType(type);
@@ -178,11 +179,7 @@ function Users() {
   const SpinnerComponent = () => {
     return (
       <div className="flex justify-center items-center m-4 ">
-        <Spinner
-          color="warning"
-          size="lg"
-          className="flex justify-center items-center "
-        />
+        <Spinner color="warning" size="lg" className="flex justify-center items-center " />
       </div>
     );
   };
@@ -193,9 +190,7 @@ function Users() {
         <div className="flex h-screen items-center ">
           <div className="w-full h-5/6 m-6 bg-white rounded-3xl p-6">
             <div className="justify-between flex">
-              <h1 className="text-4xl text-center w-1/2 my-3">
-                Gestión de usuarios
-              </h1>
+              <h1 className="text-4xl text-center w-1/2 my-3">Gestión de usuarios</h1>
 
               <Button
                 pill
@@ -209,141 +204,140 @@ function Users() {
               <ModalComponent
                 show={showModal}
                 onClose={() => setShowModal(!showModal)}
-                header={`${
-                  currentType == "create" ? "Crear" : "Editar"
-                } Usuario`}
+                header={`${currentType == "create" ? "Crear" : "Editar"} Usuario`}
                 body={<BodyModalComponent />}
                 footer={""}
               />
             </div>
             <div>
-            <div 
-              className="flex text-center justify-center gap-3.5 my-4">
-            <Button
-              size="xs"
-              className="bg-[var(--primary)]"
-              onClick={() => applyFilter(null)}
-            > 
-            Mostrar Todos
-            </Button>
-            <Button 
-              size="xs"
-              className="bg-[var(--primary)]"
-              onClick={() => applyFilter(1)}
-            >
-            Administrador
-            </Button>
+              <div className="flex justify-between my-4">
+                <div className="w-1/2 flex text-center gap-3.5 justify-center">
+                  <Button
+                    size="xs"
+                    className="bg-[var(--primary)]"
+                    onClick={() => applyFilter(null)}
+                  >
+                    Mostrar Todos
+                  </Button>
+                  <Button size="xs" className="bg-[var(--primary)]" onClick={() => applyFilter(1)}>
+                    Administrador
+                  </Button>
 
-            <Button 
-              size="xs"
-              className="bg-[var(--primary)]"
-              onClick={() => applyFilter(2)}
-            >
-            Doctores
-            </Button>
-            
-            <Button 
-              size="xs"
-              className="bg-[var(--primary)]"
-              onClick={() => applyFilter(3)}
-            >
-            Pacientes
-            </Button>
+                  <Button size="xs" className="bg-[var(--primary)]" onClick={() => applyFilter(2)}>
+                    Doctores
+                  </Button>
 
-            </div>
-            <div className="overflow-y-scroll">
-              {loadingPage ? (
-                <SpinnerComponent />
-              ) : (
-                <Table>
-                  <Table.Head className="text-center">
-                    <Table.HeadCell>Documento</Table.HeadCell>
-                    <Table.HeadCell>Nombre</Table.HeadCell>
-                    <Table.HeadCell>Apellido</Table.HeadCell>
-                    <Table.HeadCell>Celular</Table.HeadCell>
-                    <Table.HeadCell>Correo</Table.HeadCell>
-                    <Table.HeadCell>Rol</Table.HeadCell>
-                    <Table.HeadCell>Acciones</Table.HeadCell>
-                  </Table.Head>
+                  <Button size="xs" className="bg-[var(--primary)]" onClick={() => applyFilter(3)}>
+                    Pacientes
+                  </Button>
+                </div>
+                <div className="w-1/2 text-right flex text-center justify-center gap-3.5">
+                  <TextInput
+                    sizing="sm"
+                    type="text"
+                    placeholder="Buscar por numero de documento"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                  />
+                  <Button size="xs" className="bg-[var(--primary)]">
+                    Buscar
+                  </Button>
+                </div>
+              </div>
 
-                  <Table.Body className="divide-y">
-                    {users.filter((e) => filterRolId === null || e.rolId === filterRolId).map((e) => (
-                      <Table.Row
-                        key={e.rolId}
-                        className="bg-white dark:border-gray-700 dark:bg-gray-800 text-center"
-                      >
-                        <Table.Cell>{e.document}</Table.Cell>
-                        <Table.Cell>{e.name}</Table.Cell>
-                        <Table.Cell>{e.lastName}</Table.Cell>
-                        <Table.Cell>{e.cellPhone}</Table.Cell>
-                        <Table.Cell>{e.email}</Table.Cell>
-                        <Table.Cell>{getRol(e.rolId)}</Table.Cell>
-                        <Table.Cell>
-                          <div className="flex text-center justify-center">
-                            <Button
-                              size="xs"
-                              pill
-                              color="warning"
-                              className="mx-2"
-                              onClick={() => {
-                                setCurrentUser(e);
-                                handleToggleModal("edit", e);
-                              }}
-                            >
-                              Editar
-                            </Button>
+              <div className="overflow-y-scroll">
+                {loadingPage ? (
+                  <SpinnerComponent />
+                ) : (
+                  <Table>
+                    <Table.Head className="text-center">
+                      <Table.HeadCell>Documento</Table.HeadCell>
+                      <Table.HeadCell>Nombre</Table.HeadCell>
+                      <Table.HeadCell>Apellido</Table.HeadCell>
+                      <Table.HeadCell>Celular</Table.HeadCell>
+                      <Table.HeadCell>Correo</Table.HeadCell>
+                      <Table.HeadCell>Rol</Table.HeadCell>
+                      <Table.HeadCell>Acciones</Table.HeadCell>
+                    </Table.Head>
 
-                            <Button
-                              size="xs"
-                              pill
-                              color="failure"
-                              className="mx-2"
-                              onClick={() => setOpenModal("pop-up")}
-                            >
-                              Eliminar
-                            </Button>
-                            <Modal //este modal es el de eliminar
-                              show={openModal === "pop-up"}
-                              size="md"
-                              popup
-                              onClose={() => setOpenModal(undefined)}
-                            >
-                              <Modal.Header />
-                              <Modal.Body>
-                                <div className="text-center">
-                                  <LiaExclamationCircleSolid className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
-                                  <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                                    ¿Está seguro que quiere eliminar este
-                                    registro?
-                                  </h3>
-                                  <div className="flex justify-center gap-4">
-                                    <Button
-                                      color="failure"
-                                      onClick={() => {
-                                        setOpenModal(undefined);
-                                        toDelete(e);
-                                      }}
-                                    >
-                                      Si, continuar
-                                    </Button>
-                                    <Button
-                                      color="gray"
-                                      onClick={() => setOpenModal(undefined)}
-                                    >
-                                      No, cancelar
-                                    </Button>
-                                  </div>
-                                </div>
-                              </Modal.Body>
-                            </Modal>
-                          </div>
-                        </Table.Cell>
-                      </Table.Row>
-                    ))}
-                  </Table.Body>
-                </Table>
-              )}
-            </div>
+                    <Table.Body className="divide-y">
+                      {users.filter((user) =>(filterRolId === null || user.rolId === filterRolId) &&
+                            (searchTerm === "" ||user.document.toLowerCase().includes(searchTerm.toLowerCase()))).map((e) => (
+                          <Table.Row
+                            key={e.rolId}
+                            className="bg-white dark:border-gray-700 dark:bg-gray-800 text-center"
+                          >
+                            <Table.Cell>{e.document}</Table.Cell>
+                            <Table.Cell>{e.name}</Table.Cell>
+                            <Table.Cell>{e.lastName}</Table.Cell>
+                            <Table.Cell>{e.cellPhone}</Table.Cell>
+                            <Table.Cell>{e.email}</Table.Cell>
+                            <Table.Cell>{getRol(e.rolId)}</Table.Cell>
+                            <Table.Cell>
+                              <div className="flex text-center justify-center">
+                                <Button
+                                  size="xs"
+                                  pill
+                                  color="warning"
+                                  className="mx-2"
+                                  onClick={() => {
+                                    setCurrentUser(e);
+                                    handleToggleModal("edit", e);
+                                  }}
+                                >
+                                  Editar
+                                </Button>
+
+                                <Button
+                                  size="xs"
+                                  pill
+                                  color="failure"
+                                  className="mx-2"
+                                  onClick={() => setOpenModal("pop-up")}
+                                >
+                                  Eliminar
+                                </Button>
+                                <Modal //este modal es el de eliminar
+                                  show={openModal === "pop-up"}
+                                  size="md"
+                                  popup
+                                  onClose={() => setOpenModal(undefined)}
+                                >
+                                  <Modal.Header />
+                                  <Modal.Body>
+                                    <div className="text-center">
+                                      <LiaExclamationCircleSolid className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                                      <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                                        ¿Está seguro que quiere eliminar este registro?
+                                      </h3>
+                                      <div className="flex justify-center gap-4">
+                                        <Button
+                                          color="failure"
+                                          onClick={() => {
+                                            setOpenModal(undefined);
+                                            toDelete(e);
+                                          }}
+                                        >
+                                          Si, continuar
+                                        </Button>
+                                        <Button
+                                          color="gray"
+                                          onClick={() => setOpenModal(undefined)}
+                                        >
+                                          No, cancelar
+                                        </Button>
+                                      </div>
+                                    </div>
+                                  </Modal.Body>
+                                </Modal>
+                              </div>
+                            </Table.Cell>
+                          </Table.Row>
+                        ))}
+                    </Table.Body>
+                  </Table>
+                )}
+              </div>
             </div>
           </div>
         </div>
