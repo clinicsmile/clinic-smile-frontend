8a5fce59b9c67a9dc4515dec2750ffeeb974be64
Daniel Otalora <Daniel.otalora@pcpplasticos.com>
2023-10-30 14:15:44 -0500

    try build

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    host:"192.168.1.102",
-    port:"5173"
-  },
   plugins: [react()],
   esbuild: {
     logOverride: { 'this-is-undefined-in-esm': 'silent' }
