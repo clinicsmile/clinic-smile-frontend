124b0cef626bcc15867c4fd1d9e77f8b19521504
Daniel Otalora <Daniel.otalora@pcpplasticos.com>
2023-11-14 21:20:49 -0500

    Refactor Users component layout

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -8,7 +8,6 @@ import { services } from "../../services/services";
 import Swal from "sweetalert2";
 import ModalComponent from "../../components/modal/Modal";
 
-
 function Users() {
   const [openModal, setOpenModal] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -222,9 +221,7 @@ function Users() {
         <div className="h-screen flex">
           <div className="w-5/6 h-5/6 bg-white rounded-3xl text-center m-auto shadow-black shadow-2xl">
             <div className="m-auto p-6 ">
-
               <div className="p-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 ">
-
                 <div className="m-auto my-3 ">
                   <h1 className="font-serif text-4xl text-center grid-cols-1 border-b-2 border-b-[--primary] pb-3">
                     Gestión de usuarios
@@ -289,9 +286,7 @@ function Users() {
                 {loadingPage ? (
                   <SpinnerComponent />
                 ) : (
-                  <div
-                    className="px-3 responsive-div"
-                  >
+                  <div className="px-3 responsive-div">
                     <Table className="text-center p-6">
                       <Table.Head>
                         <Table.HeadCell className="bg-[--primary] text-white">
